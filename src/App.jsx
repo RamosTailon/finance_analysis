e4b37a71fd5c7f921d8267580ffe00813bb2edb5
Tailon Ramos <tailonramos11@gmail.com>
2023-02-04 20:41:30 -0300

    testes da biblioteca React Flow

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,136 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect, useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge, Controls } from 'reactflow';
 
-function App() {
-  const [count, setCount] = useState(0)
+import 'reactflow/dist/style.css';
 
+import ColorSelectorNode from './nodes/ColorSelectorNode';
+
+import './index.css';
+
+const initBgColor = '#1A192B';
+
+const connectionLineStyle = { stroke: '#fff' };
+const snapGrid = [20, 20];
+const nodeTypes = {
+  selectorNode: ColorSelectorNode,
+};
+
+const defaultViewport = { x: 0, y: 0, zoom: 1.5 };
+
+const App = () => {
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [bgColor, setBgColor] = useState(initBgColor);
+
+  useEffect(() => {
+    const onChange = (event) => {
+      setNodes((nds) =>
+        nds.map((node) => {
+          if (node.id !== '2') {
+            return node;
+          }
+
+          const color = event.target.value;
+
+          setBgColor(color);
+
+          return {
+            ...node,
+            data: {
+              ...node.data,
+              color,
+            },
+          };
+        })
+      );
+    };
+
+    setNodes([
+      {
+        id: '1',
+        type: 'output',
+        data: { label: 'Investimento A' },
+        position: { x: 650, y: 150 },
+        targetPosition: 'left',
+      },
+      {
+        id: '2',
+        type: 'selectorNode',
+        data: { onChange: onChange, color: initBgColor },
+        style: { border: '1px solid #777', padding: 10 },
+        position: { x: 300, y: 50 },
+      },
+      {
+        id: '3',
+        type: 'output',
+        data: { label: 'Retorno A' },
+        position: { x: 650, y: 25 },
+        targetPosition: 'left',
+      },
+      {
+        id: '4',
+        type: 'output',
+        data: { label: 'Retorno B' },
+        position: { x: 650, y: 100 },
+        targetPosition: 'left',
+      },
+    ]);
+
+    setEdges([
+      {
+        id: 'e1a-2',
+        source: '2',
+        target: '1',
+        sourceHandle: 'a',
+        animated: true,
+        style: { stroke: '#fff' },
+      },
+      {
+        id: 'e2a-3',
+        source: '2',
+        target: '3',
+        sourceHandle: 'a',
+        animated: true,
+        style: { stroke: '#fff' },
+      },
+      {
+        id: 'e2b-4',
+        source: '2',
+        target: '4',
+        sourceHandle: 'b',
+        animated: true,
+        style: { stroke: '#fff' },
+      },
+    ]);
+  }, []);
+
+  const onConnect = useCallback(
+    (params) =>
+      setEdges((eds) => addEdge({ ...params, animated: true, style: { stroke: '#fff' } }, eds)),
+    []
+  );
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    <div className='container'>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        style={{ background: bgColor }}
+        nodeTypes={nodeTypes}
+        connectionLineStyle={connectionLineStyle}
+        snapToGrid={true}
+        snapGrid={snapGrid}
+        defaultViewport={defaultViewport}
+        fitView
+        attributionPosition="bottom-left"
+      >
 
-export default App
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
+};
+
+export default App;
